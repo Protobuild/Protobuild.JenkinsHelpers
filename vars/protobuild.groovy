103b1f9091734389efcb25da5f357ba0c0400ed2
June Rhodes <jrhodes@redpointgames.com.au>
2016-09-18 00:25:32 +1000

    Remove redundant parameters

--- vars/protobuild.groovy
+++ vars/protobuild.groovy
@@ -1,42 +1,30 @@
 import org.protobuild.ProtobuildBuilder
 
-def call(name, url) {
-  //stage(name) {
-    //def platforms = [:]
-
-    //platforms['linux'] = {
-    stage(name + ": Linux") {
-      node('linux') {
-        timeout(30) {
-          checkout poll: true, changelog: true, scm: scm
-          sh ("mono Protobuild.exe --automated-build")
-        }
+def call() {
+  stage("Linux") {
+    node('linux') {
+      timeout(30) {
+        checkout poll: true, changelog: true, scm: scm
+        sh ("mono Protobuild.exe --automated-build")
       }
     }
-    //}
+  }
 
-    //platforms['mac'] = {
-    stage(name + ": Mac") {
-      node('mac') {
-        timeout(30) {
-          checkout poll: false, changelog: false, scm: scm
-          sh ("/usr/local/bin/mono Protobuild.exe --automated-build")
-        }
+  stage("Mac") {
+    node('mac') {
+      timeout(30) {
+        checkout poll: false, changelog: false, scm: scm
+        sh ("/usr/local/bin/mono Protobuild.exe --automated-build")
       }
     }
-    //}
-
-    //platforms['windows'] = {
-    stage(name + ": Windows") {
-      node('windows') {
-        timeout(30) {
-          checkout poll: false, changelog: false, scm: scm
-          bat ('Protobuild.exe --automated-build')
-        }
+  }
+    
+  stage("Windows") {
+    node('windows') {
+      timeout(30) {
+        checkout poll: false, changelog: false, scm: scm
+        bat ('Protobuild.exe --automated-build')
       }
     }
-    //}
-
-    //parallel platforms
-  //}
+  }
 }
