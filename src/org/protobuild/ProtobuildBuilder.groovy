0b5ae4b936fc93fe0cef676d602b85b4f29dad43
June Rhodes <jrhodes@redpointgames.com.au>
2016-09-17 16:10:53 +1000

    Try temporarily working around serializable issue

--- src/org/protobuild/ProtobuildBuilder.groovy
+++ src/org/protobuild/ProtobuildBuilder.groovy
@@ -9,38 +9,6 @@ class ProtobuildBuilder {
   }
 
   def buildProject(name, url) {
-    steps.stage(name) {
-      def platforms = [:]
-
-      platforms['linux'] = {
-        steps.node('linux') {
-          steps.timeout(30) {
-            steps.checkout poll: true, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: name], [$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2a0f2367-7be2-419f-9b0f-b55c3305dea0', url: url]]]
-            steps.sh ("cd " + name + " && mono Protobuild.exe --automated-build")
-          }
-        }
-      }
-
-      platforms['mac'] = {
-        steps.node('mac') {
-          steps.timeout(30) {
-            steps.checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: name], [$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2a0f2367-7be2-419f-9b0f-b55c3305dea0', url: url]]]
-            steps.sh ("cd " + name + " && /usr/local/bin/mono Protobuild.exe --automated-build")
-          }
-        }
-      }
-
-      platforms['windows'] = {
-        steps.node('windows') {
-          steps.timeout(30) {
-            steps.checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: name], [$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2a0f2367-7be2-419f-9b0f-b55c3305dea0', url: url]]]
-            steps.bat ('cd ' + name + ' && Protobuild.exe --automated-build')
-          }
-        }
-      }
-
-      steps.parallel platforms
-    }
   }
 
 }
--- vars/protobuild.groovy
+++ vars/protobuild.groovy
@@ -1,6 +1,36 @@
 import org.protobuild.ProtobuildBuilder
 
 def call(name, url) {
-  def builder = new ProtobuildBuilder(steps)
-  builder.buildProject(name, url)
+  stage(name) {
+    def platforms = [:]
+
+    platforms['linux'] = {
+      node('linux') {
+        timeout(30) {
+          checkout poll: true, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: name], [$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2a0f2367-7be2-419f-9b0f-b55c3305dea0', url: url]]]
+          sh ("cd " + name + " && mono Protobuild.exe --automated-build")
+        }
+      }
+    }
+
+    platforms['mac'] = {
+      node('mac') {
+        timeout(30) {
+          checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: name], [$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2a0f2367-7be2-419f-9b0f-b55c3305dea0', url: url]]]
+          sh ("cd " + name + " && /usr/local/bin/mono Protobuild.exe --automated-build")
+        }
+      }
+    }
+
+    platforms['windows'] = {
+      node('windows') {
+        timeout(30) {
+          checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: name], [$class: 'CleanCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2a0f2367-7be2-419f-9b0f-b55c3305dea0', url: url]]]
+          bat ('cd ' + name + ' && Protobuild.exe --automated-build')
+        }
+      }
+    }
+
+    parallel platforms
+  }
 }
