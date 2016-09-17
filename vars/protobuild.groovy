9643eccf1996dce6af5b7034dc4416cac906b1e1
June Rhodes <jrhodes@redpointgames.com.au>
2016-09-17 22:37:08 +1000

    Fix SCM checkout parameters

--- vars/protobuild.groovy
+++ vars/protobuild.groovy
@@ -8,7 +8,7 @@ def call(name, url) {
     stage(name + ": Linux") {
       node('linux') {
         timeout(30) {
-          checkout poll: true, changelog: true, scm
+          checkout poll: true, changelog: true, scm: scm
           sh ("cd " + name + " && mono Protobuild.exe --automated-build")
         }
       }
@@ -19,7 +19,7 @@ def call(name, url) {
     stage(name + ": Mac") {
       node('mac') {
         timeout(30) {
-          checkout poll: false, changelog: false, scm
+          checkout poll: false, changelog: false, scm: scm
           sh ("cd " + name + " && /usr/local/bin/mono Protobuild.exe --automated-build")
         }
       }
@@ -30,7 +30,7 @@ def call(name, url) {
     stage(name + ": Windows") {
       node('windows') {
         timeout(30) {
-          checkout poll: false, changelog: false, scm
+          checkout poll: false, changelog: false, scm: scm
           bat ('cd ' + name + ' && Protobuild.exe --automated-build')
         }
       }
