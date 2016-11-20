e862bea1b9e178ce046b09b2f9b923693370f972
June Rhodes <jrhodes@redpointgames.com.au>
2016-11-21 09:51:00 +1100

    Add a unified build stage for projects to ship unified packages

--- vars/protobuild.groovy
+++ vars/protobuild.groovy
@@ -7,6 +7,7 @@ def call() {
         checkout poll: false, changelog: false, scm: scm
         bat ("Protobuild.exe --upgrade-all")
         bat ('Protobuild.exe --automated-build')
+        stash includes: '*.nupkg', name: 'windows-packages'
       }
     }
   }
@@ -17,6 +18,7 @@ def call() {
         checkout poll: false, changelog: false, scm: scm
         sh ("/usr/local/bin/mono Protobuild.exe --upgrade-all")
         sh ("/usr/local/bin/mono Protobuild.exe --automated-build")
+        stash includes: '*.nupkg', name: 'mac-packages'
       }
     }
   }
@@ -27,6 +29,25 @@ def call() {
         checkout poll: true, changelog: true, scm: scm
         sh ("mono Protobuild.exe --upgrade-all")
         sh ("mono Protobuild.exe --automated-build")
+        stash includes: '*.nupkg', name: 'linux-packages'
+      }
+    }
+  }
+
+  stage("Unified") {
+    node('linux') {
+      timeout(30) {
+        // Ensure a seperate working directory to the normal linux node above.
+        ws {
+          checkout poll: true, changelog: true, scm: scm
+          if (fileExists('unified.build')) {
+            sh ("mono Protobuild.exe --upgrade-all")
+            unstash 'windows-packages'
+            unstash 'mac-packages'
+            unstash 'linux-packages'
+            sh ("mono Protobuild.exe --automated-build unified.build")
+          }
+        }
       }
     }
   }
